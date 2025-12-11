59ab5f63b9380ec65ff703bcb0fc835ac4939ea9
Sharegh Yusefi <ShareghYusefi@gmail.com>
2025-12-10 19:44:43 -0700

    reactive form validation

--- src/app/components/task-form/task-form.component.ts
+++ src/app/components/task-form/task-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { Task } from '../../interfaces/task';
 import { TaskService } from 'src/app/services/task-service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-task-form',
@@ -10,27 +11,41 @@ import { TaskService } from 'src/app/services/task-service';
   styleUrls: ['./task-form.component.scss'],
 })
 export class TaskFormComponent implements OnInit {
-  task: Task = {
-    id: 0,
-    title: '',
-    description: '',
-    priority_level: 'Low',
-  };
+  taskForm!: FormGroup;
 
   constructor(
     private navCtrl: NavController,
-    private taskService: TaskService
+    private taskService: TaskService,
+    private formBuilder: FormBuilder
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.taskForm = this.formBuilder.group({
+      id: [0, [Validators.required]],
+      title: ['', [Validators.required, Validators.minLength(3)]],
+      description: ['', [Validators.required]],
+      priority_level: ['Low', [Validators.required]],
+    });
+  }
+
+  // getters for displaying errors
+  get title() {
+    return this.taskForm.get('title');
+  }
+  get description() {
+    return this.taskForm.get('description');
+  }
+  get priority_level() {
+    return this.taskForm.get('priority_level');
+  }
 
   closeForm() {
     this.navCtrl.navigateBack('/tabs/tab1');
   }
 
   onSubmit() {
-    console.log('Form submitted!', this.task);
-    this.taskService.addTask(this.task).subscribe(
+    console.log('Form submitted!', this.taskForm.value);
+    this.taskService.addTask(this.taskForm.value).subscribe(
       (result: Task) => {
         console.log('Task added!', result);
       },
